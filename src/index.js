dc26c09ce77dbbc1937e3bf051448ab7a8833e69
Spencer <spencer.adler2@gmail.com>
2024-05-21 13:40:46 -0400

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    handleToDo(e.target[0].value)
+  })
 });
+
+function handleToDo(newTask){
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', handleDelete)
+  btn.textContent = 'x'
+  p.textContent = `${newTask} `
+  p.appendChild(btn)
+  document.querySelector('#tasks').appendChild(p)
+}
+
+function handleDelete(e){
+  e.target.parentNode.remove()
+}
